d086a80f3e618154d513cb26e9a4ebb8b51b24e3
Unknown <joeohara98@gmail.com>
2017-11-21 17:57:22 -0500

    Final Project Update 4
    updates before final class presentation

--- FINAL Project/js/index.js
+++ FINAL Project/js/index.js
@@ -4,4 +4,20 @@ $('.hamburgerMenu').click(function() {
 	$('.menu').slideToggle('800');
 });
 
+    //Window scroll
+$("nav.toggleNav a").click(function(e) {
+    var id = $(this).attr('href');
+
+     $('html, body').animate({
+        scrollTop: $(id).offset().top
+     }, 2000);
+
+       e.preventDefault();
+});
+
+    $('a.external-url').live('click', function(e){
+  window.open(this.href);
+  e.preventDefault(); //or return false;
+});
+
 })
